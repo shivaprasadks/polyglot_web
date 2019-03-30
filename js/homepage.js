e004954ceb16c41007dd1e370382ab78602b6005
shivaprasad.ks <shivaprasad.ks@flipkart.com>
2019-03-30 16:28:48 +0530

    Slection box addition

--- js/homepage.js
+++ js/homepage.js
@@ -1,8 +1,11 @@
- document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, options);
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+    var instance = M.FormSelect.getInstance(elem);
   });
 
   // Or with jQuery
 
-  $('.dropdown-trigger').dropdown();
+//   $('.dropdown-trigger').dropdown();
+
+  console.log('sdfghjkl')
