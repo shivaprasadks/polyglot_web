0f2d8c8256de003bf9f5f6d0dc389018c7ab5c66
shivaprasad.ks <shivaprasad.ks@flipkart.com>
2019-04-01 05:02:54 +0530

    Added php files

--- js/homepage.js
+++ js/homepage.js
@@ -1,7 +1,25 @@
 //using vanilla js for the better understanding
 (function () {
     // private
-    document.getElementById('pushMe').addEventListener('click',function(e){
-        var source = document.getElementById('sourceCode').value;
+    document.getElementById('pushMe').addEventListener('click',function(){
+        let sourceCode = document.getElementById('sourceCode').value;
+        let e = document.getElementById('langList'),
+        langCode = e.options[e.selectedIndex].value;
+        
+      
+
+       var url = 'http://localhost/getData.php';
+        var remoteUrl='http://13.234.128.68/getData.php';
+
+        // fetch('http://13.234.128.68/getData.php', {
+        fetch(url, {
+            method: 'post',
+            credentials: "same-origin", 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({source: sourceCode, lang: langCode})
+            }).then(res=>res.text())
+            .then(res => document.getElementById('myOutput').innerHTML =res);
     })
   }());
